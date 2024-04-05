1fa57cb811499fb470c77056b1afb9cc9aedb285
tomasredu <166166896+tomasredu@users.noreply.github.com>
2024-04-05 15:50:32 -0300

    2.3

---
+++ 412012-Punto/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _412012_Punto
+{
+    internal class Punto
+    {
+        private double valorX;
+        private double valorY;
+
+        public double pValorX
+        {
+            get { return valorX;}
+            set { valorX = value;}
+        }
+        public double pValorY { 
+            get { return valorY;} 
+            set { valorY = value; } 
+        }
+
+        public Punto()
+        {
+            valorX = 0;
+            valorY = 0;
+        }
+        public Punto(double valorX, double valorY)
+        {
+            this.valorX = valorX;
+            this.valorY = valorY;
+        }
+
+        public double getModulo()
+        {
+            return Math.Sqrt(valorY * valorY + valorX * valorX);
+        }
+
+        public void mostrarPunto()
+        {
+            string sPunto = "(" + valorX + "," + valorY + ")";
+            Console.WriteLine(sPunto);
+        }
+
+}
+}
---
+++ 412012-Punto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _412012_Punto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto punto= new Punto(2,4);
+            punto.mostrarPunto();
+            Console.WriteLine(punto.getModulo());
+            Console.Read();
+        }
+    }
+    
+}
